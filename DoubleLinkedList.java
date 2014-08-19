22424caec20dbf910b031671515700e13d2268cf
Luis Alberto Durazo Arvizu <ldurazo@nearsoft.com>
2014-08-18 18:43:11 -0700

    Finished Double linked list

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.NoSuchElementException;
+
 public class DoubleLinkedList<Item> {
     private Node<Item> head = null;
     private Node<Item> back = null;
@@ -15,22 +17,6 @@ public class DoubleLinkedList<Item> {
         return counter == 0;
     }
 
-    public void insertAtEnd(Item item) {
-        Node<Item> pointer = new Node<Item>();
-        pointer.item = item;
-        if (isEmpty()) {
-            head = pointer;
-            back = head;
-        } else {
-            holder = head.previous;
-            head.previous = holder.previous;
-            holder.next = head.previous;
-            head.next = pointer;
-            head = pointer;
-        }
-        counter++;
-    }
-
     public void insertAtStart(Item item) {
         Node<Item> pointer = new Node<Item>();
         pointer.item = item;
@@ -38,9 +24,23 @@ public class DoubleLinkedList<Item> {
             head = pointer;
             back = head;
         } else {
-            pointer.previous = head;
-            pointer.next = null;
+            pointer.next = head;
+            head.previous = pointer;
             head = pointer;
+        }
+        counter++;
+    }
+
+    public void insertAtEnd(Item item) {
+        Node<Item> pointer = new Node<Item>();
+        pointer.item = item;
+        if (isEmpty()) {
+            head = pointer;
+            back = head;
+        } else {
+            pointer.previous = back;
+            back.next = pointer;
+            back = pointer;
         }
         counter++;
     }
@@ -55,39 +55,55 @@ public class DoubleLinkedList<Item> {
             return;
         }
         holder = head;
-        for (int i = 0; i < index - 1; i++) {
+        for (int i = 0; i < index ; i++) {
             holder = holder.next;
         }
-        Node<Item> node = new Node<Item>();
-        node.item = item;
-        node.next = holder.next;
-        node.previous = holder.previous;
-        holder.next = node;
+        Node<Item> pointer = new Node<Item>();
+        pointer.item = item;
+        pointer.next = holder;
+        pointer.previous = holder.previous;
+        holder.previous = pointer;
+        Node<Item> itemNode;
+        itemNode=pointer.previous;
+        itemNode.next = pointer;
         counter++;
     }
 
-    public Item getItem(int index) {
-        if (index >= 0 && index < size()) {
+    public Item getItem(int index){
+        if (index >= 0 && index <= size()) {
             holder = head;
             for (int i = 0; i < index; i++) {
                 holder = holder.next;
             }
             return holder.item;
+        }else{
+            throw new IndexOutOfBoundsException();
         }
-        return null;
     }
 
-    public int indexOf(Item item) {
-        holder = back;
-        for (int i = size(); i > 0; i--) {
-            if (holder.item.equals(item)) return i;
-            holder = holder.previous;
+    public Item getItemInReverse(int index){
+        if (index >= 0 && index <= size()) {
+            holder = back;
+            for (int i = index; i > 0; i--) {
+                holder = holder.previous;
+            }
+            return holder.item;
+        }else{
+            throw new IndexOutOfBoundsException();
         }
-        return -1;
+    }
+
+    public int indexOf(Item item){
+        holder = head;
+        for (int i = 0; i < size(); i++) {
+            if (holder.item.equals(item)) return i;
+            holder = holder.next;
+        }
+        throw new NoSuchElementException(" No such element ");
     }
 
     public Item remove(int index) {
-        if (index >= 0 && index < size()) {
+        if (index >= 0 && index <= size()) {
             holder = head;
             if (index == 0) {
                 Item item = head.item;
@@ -102,23 +118,22 @@ public class DoubleLinkedList<Item> {
                 counter--;
                 return item;
             }
-            for (int i = 0; i < index - 1; i++) {
+            for (int i = 0; i < index ; i++) {
                 holder = holder.next;
             }
-            Node<Item> temporaryNode = holder.next;
-            holder.next = temporaryNode.next;
-            temporaryNode.previous = holder.previous;
-            holder.previous = temporaryNode.previous;
-            Item item = temporaryNode.item;
-            temporaryNode = null;
-            counter --;
+            Item item = holder.item;
+            Node<Item> nextNode = holder.next;
+            nextNode.previous = holder.previous;
+            Node<Item> prevNode = holder.previous;
+            prevNode.next = holder.next;
+            counter--;
             return item;
+        }else{
+            throw new IndexOutOfBoundsException();
         }
-        return null;
     }
 
     public Item remove(Item item){
-
-        return null;
+        return remove(indexOf(item));
     }
 }
--- Main.java
+++ Main.java
@@ -4,16 +4,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
         DoubleLinkedList<Object> dlinkedList = new DoubleLinkedList<Object>();
-        LinkedList<Object> linkedList = new LinkedList<Object>();
-
         dlinkedList.insertAtStart("head");
-        dlinkedList.insertAtStart("one");
-        dlinkedList.insertAtStart("two");
+        dlinkedList.insertAtStart("7");
+        dlinkedList.insertAtStart("6");
+        dlinkedList.insertAtStart("5");
+        dlinkedList.insertAtStart("4");
+        dlinkedList.insertAtStart("3");
+        dlinkedList.insertAtStart("2");
         dlinkedList.insertAtStart("back");
+        dlinkedList.insertAt(7, "insertAt");
+
+        System.out.println(String.valueOf(dlinkedList.indexOf("insertAt")));
+        dlinkedList.remove("insertAt");
+        System.out.println("----------------------------->");
         for (int i = 0; i < dlinkedList.size(); i++) {
             System.out.println(dlinkedList.getItem(i));
+        }
+        System.out.println("----------------------------->");
+        for (int i = 0; i < dlinkedList.size(); i++) {
+            System.out.println(dlinkedList.getItemInReverse(i));
         }
     }
 }
